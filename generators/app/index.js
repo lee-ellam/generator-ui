495b3ebfe42af3b554bc1f5f0805eca436410a14
Lee Ellam <lee.ellam@justgiving.com>
2015-06-16 15:35:19 +0100

    add all project files from example module

---
+++ generators/app/index.js
@@ -0,0 +1,220 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var mkdirp = require('mkdirp');
+
+module.exports = yeoman.generators.Base.extend({
+  constructor: function() {
+    yeoman.generators.Base.apply(this, arguments);
+
+    this.option('no-install', {
+      type: Boolean,
+      required: false,
+      defaults: false,
+      desc: 'When specified, npm dependencies are not installed'
+    });
+  },
+
+  promptUser: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.blue('JustGiving UI module') + ' generator!'
+    ));
+
+    var prompts = [{
+        name: 'projectName',
+        message: 'What\'s the name of your module?'
+      },
+      {
+        name: 'projectDescription',
+        message: 'Give a short description of your module'
+      },
+      {
+        name: 'githubOrganisation',
+        message: 'What is your GitHub organisation?',
+        default: 'JustGiving'
+      },
+      {
+        name: 'githubUsername',
+        message: 'What is your GitHub username?'
+      },
+      {
+        type: 'list',
+        name: 'projectType',
+        message: 'What type of module do you want to create?',
+        choices: [{
+            name: 'Empty',
+            value: 'empty'
+          },
+          {
+            name: 'CSS',
+            value: 'css'
+          },
+          {
+            name: 'JavaScript',
+            value: 'js'
+          },
+          {
+            name: 'Template',
+            value: 'template'
+          },
+          {
+            name: 'Component (all of the above)',
+            value: 'component'
+        }]
+      }
+    ];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      this.projectType = props.projectType;
+      this.projectName = props.projectName;
+      this.projectDescription = props.projectDescription;
+      this.githubUsername = props.githubUsername;
+      this.githubOrganisation = props.githubOrganisation;
+
+      done();
+    }.bind(this));
+  },
+
+  moduleFilesAndDirs: function() {
+    // create dirs
+    var done = this.async();
+
+    mkdirp('src');
+
+    switch (this.projectType) {
+      case 'empty':
+        break;
+      case 'css':
+        mkdirp('src/less');
+        this.copy(
+          this.templatePath('src/less/index.less'),
+          this.destinationPath('src/less/index.less')
+        );
+        break;
+      case 'js':
+        mkdirp('src/js');
+        mkdirp('test');
+        this.copy(
+          this.templatePath('src/js/index.js'),
+          this.destinationPath('src/js/index.js')
+        );
+        break;
+      case 'template':
+        mkdirp('src/templates');
+        this.copy(
+          this.templatePath('src/templates/index.html'),
+          this.destinationPath('src/templates/index.html')
+        );
+        break;
+      case 'component':
+        mkdirp('src/less');
+        mkdirp('src/js');
+        mkdirp('src/templates');
+        mkdirp('test');
+        this.copy(
+          this.templatePath('src/less/index.less'),
+          this.destinationPath('src/less/index.less')
+        );
+        this.copy(
+          this.templatePath('src/js/index.js'),
+          this.destinationPath('src/js/index.js')
+        );
+        this.copy(
+          this.templatePath('src/templates/index.html'),
+          this.destinationPath('src/templates/index.html')
+        );
+        break;
+    }
+    done();
+  },
+
+  projectRootFiles: function () {
+    // gitignore
+    this.copy(
+      this.templatePath('gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    // editorconfig
+    this.copy(
+      this.templatePath('editorconfig'),
+      this.destinationPath('.editorconfig')
+    );
+
+    // jshint
+    this.copy(
+      this.templatePath('jshintrc'),
+      this.destinationPath('.jshintrc')
+    );
+
+    // license
+    this.template(
+      this.templatePath('license'),
+      this.destinationPath('license'),
+      {
+        githubOrganisation: this.githubOrganisation
+      }
+    );
+
+    // readme
+    this.template(
+      this.templatePath('_readme.md'),
+      this.destinationPath('readme.md'),
+      {
+        projectName: this.projectName,
+        projectDescription: this.projectDescription,
+        githubUsername: this.githubUsername,
+        githubOrganisation: this.githubOrganisation,
+        githubProjectName: this.projectName
+      }
+    );
+
+    // package.json
+    this.template(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      {
+        projectName: this.projectName,
+        projectDescription: this.projectDescription,
+        githubUsername: this.githubUsername,
+        githubOrganisation: this.githubOrganisation,
+        githubProjectName: this.projectName
+      }
+    );
+
+    // bower.json
+    this.template(
+      this.templatePath('_bower.json'),
+      this.destinationPath('bower.json'),
+      {
+        projectName: this.projectName,
+        projectDescription: this.projectDescription,
+        githubUsername: this.githubUsername,
+        githubOrganisation: this.githubOrganisation,
+        githubProjectName: this.projectName
+      }
+    );
+
+    // gulpfile
+    this.copy(
+      this.templatePath('_gulpfile.js'),
+      this.destinationPath('gulpfile.js')
+    );
+
+    // karma
+    this.copy(
+      this.templatePath('_karma.conf.js'),
+      this.destinationPath('karma.conf.js')
+    );
+  },
+
+  install: function () {
+    if (this.options['no-install']) return;
+    this.installDependencies();
+  }
+});
---
+++ generators/app/templates/_readme.md
@@ -0,0 +1,32 @@
+# <%= projectName %>
+> <%= projectDescription %>
+
+## Usage
+Add some info on usage here.
+
+## Contributing
+[![dependency Status][dependency-image]][dependency-url]
+[![devDependency Status][dev-dependency-image]][dev-dependency-url]
+
+To contribute check the [GitHub issues](https://github.com/<%= githubUsername %>/<%= _.slugify(githubProjectName) %>/issues) then work away:
+
+1. [Fork it](https://github.com/<%= githubOrganisation %>/<%= _.slugify(githubProjectName) %>/fork)!
+2. Create your feature branch: `git checkout -b feature/awesome-feature`
+3. Run: `npm install` for dependencies and `npm link` for local development
+4. Commit your changes: `git commit -m 'feat(Project): Adds awesome feature'`
+5. Push to the branch: `git push origin feature/awesome-feature`
+6. Submit a [pull request](https://github.com/<%= githubUsername %>/<%= _.slugify(githubProjectName) %>/pulls) :+1:
+
+Tips:
+* In lieu of a formal styleguide, take care to maintain the existing coding style.
+* Add unit tests for any new or changed functionality.
+* Test your code using [Jasmine](http://jasmine.github.io/2.0/introduction.html) via [Karma](http://karma-runner.github.io/0.12/index.html): `npm test`
+* Commit messages loosely adhere to [these guidelines](https://github.com/angular/angular.js/blob/master/CONTRIBUTING.md#commit).
+* Versioning adheres to [Semver](http://semver.org).
+
+## Release History
+* _Nothing yet_
+
+## License
+
+MIT: http://mit-license.org/
